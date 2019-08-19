a91efd51c3e1ac59c58344abd6793efcf6d0d3ca
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-19 19:53:42 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,9 +23,7 @@ def end_game(card_total)
 end
 
 def initial_round
-  card_1 = deal_card
-  card_2 = deal_card
-  cards = card_1 + card_2
+ cards = 2.times{deal_card} 
   display_card_total(cards)
   return cards 
 end
